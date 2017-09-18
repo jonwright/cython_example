f7b2700ef7237942f7ff2900647b4c09490fa462
jonwright <wright@esrf.fr>
2017-09-18 17:57:53 +0200

    Static schedule

--- my_c_code.c
+++ my_c_code.c
@@ -6,7 +6,7 @@
 
 void vnorm(double vecs[][3], double mods[],  int n){
   int i;
-  #pragma omp parallel for
+#pragma omp parallel for schedule(static)
   for(i=0; i<n; i++){
       mods[i] = sqrt( vecs[i][0]*vecs[i][0] + 
                       vecs[i][1]*vecs[i][1] + 
